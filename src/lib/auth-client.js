3ebccc01085cac0155cdad638a42792a30349eca
sohel <sohel634500@gmail.com>
2026-05-04 22:42:00 +0600

    problem solved

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,8 +1,8 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000"
-    // baseURL: "https://assignment-08-pi.vercel.app"
+    // baseURL: "http://localhost:3000"
+    baseURL: "https://assignment-08-iota-three.vercel.app"
 })
 
 
